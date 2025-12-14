7b17b60a012192b1689df7bd5f7e3d5329da50f6
Thalisson <thalissonmoura138@gmail.com>
2025-12-14 17:25:26 -0300

    :recycle:refactor: Remover serialVersionUID da classe DefaultApiException

--- src/main/java/com/br/eventsync/exception/DefaultApiException.java
+++ src/main/java/com/br/eventsync/exception/DefaultApiException.java
@@ -1,9 +1,6 @@
 package com.br.eventsync.exception;
 
 public class DefaultApiException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
     public DefaultApiException(String msg) {
         super(msg);
     }
